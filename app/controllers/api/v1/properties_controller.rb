f22890a783d60f38427cdee9812e4cfd88b13f93
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2023-10-25 05:43:17 +0800

    setup basic routing and rspec

---
+++ app/controllers/api/v1/properties_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::PropertiesController < Api::V1::BaseController	
+	def index
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+end
---
+++ spec/routing/api/v1/properties_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::PropertiesController, type: :routing do
+  base_folder = 'api/v1'
+
+  it 'routes to index when hits GET /api/v1/properties' do
+    expect(get("/#{base_folder}/properties")).to route_to({controller: "#{base_folder}/properties", action: 'index'})
+  end
+
+  it 'routes to create when hits POST /api/v1/properties' do
+    expect(post("/#{base_folder}/properties")).to route_to({controller: "#{base_folder}/properties", action: 'create'})
+  end
+
+  it 'routes to update when hits PUT /api/v1/properties/1' do
+    expect(put("/#{base_folder}/properties/1")).to route_to({controller: "#{base_folder}/properties", action: 'update', id: '1'})
+  end
+
+  it 'routes to delete when hits DELETE /api/v1/properties/1' do
+    expect(delete("/#{base_folder}/properties/1")).to route_to({controller: "#{base_folder}/properties", action: 'destroy', id: '1'})
+  end
+
+  it 'routes to show when hits GET /api/v1/properties/1' do
+    expect(get("/#{base_folder}/properties/1")).to route_to({controller: "#{base_folder}/properties", action: 'show', id: '1'})
+  end
+end

9ace0e9c3cf2e702de35ada7a52823045458f408
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2023-10-26 17:41:17 +0800

    fix filtering

--- app/queries/property_search_query.rb
+++ app/queries/property_search_query.rb
@@ -1,13 +1,43 @@
 class PropertySearchQuery
-	include HasScope
+	attr_accessor :initial_scope
 
-  has_scope :by_city
-
-  def perform(params = {})
-    apply_scopes(collection, params)
+  def initialize(initial_scope = nil)
+    @initial_scope = initial_scope || Property.all
   end
 
-  def collection
-  	Property
+
+  def perform(params = {})
+  	scoped = initial_scope
+    scoped = filter_by_city(scoped, params[:city])
+    scoped = filter_by_district(scoped, params[:district])
+    scoped = filter_by_bedroom(scoped, params[:bedroom])
+    scoped = filter_by_rent(scoped, params[:rent_gt], params[:rent_lt])
+    scoped = filter_by_closest_mrt(scoped, params[:closest_mrt])
+  end
+
+  private
+
+  def filter_by_city(scoped, city = nil)
+  	city ? scoped.where(city: city) : scoped
+  end
+
+  def filter_by_district(scoped, district = nil)
+  	return scoped unless district
+
+  	district_list = district.split(',')
+  	scoped.where(district: district_list)
+  end
+
+  def filter_by_bedroom(scoped, bedroom = nil)
+ 		bedroom ? scoped.where('bedroom <= ?', bedroom) : scoped
+  end
+
+  def filter_by_rent(scoped, rent_gt = nil, rent_lt = nil)
+  	scoped = rent_gt ? scoped.where('rent >= ?', rent_gt) : scoped
+  	rent_lt ? scoped.where('rent <= ?', rent_lt) : scoped
+  end
+
+  def filter_by_closest_mrt(scoped, closest_mrt = nil)
+  	closest_mrt ? scoped.where(closest_mrt: closest_mrt) : scoped
   end
 end
